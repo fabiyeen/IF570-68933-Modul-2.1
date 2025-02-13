fa89fc3cac0b3814d6db6eb0669b14dc7916ee08
fabiyeen <163744056+fabiyeen@users.noreply.github.com>
2025-02-13 13:13:39 +0700

    colorschemes 7

--- app/src/main/java/com/example/reply/ui/MainActivity.kt
+++ app/src/main/java/com/example/reply/ui/MainActivity.kt
@@ -23,10 +23,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.reply.data.LocalEmailsDataProvider
 import com.example.reply.ui.theme.AppTheme
@@ -43,6 +45,7 @@ class MainActivity : ComponentActivity() {
             val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
             AppTheme {
+                Surface(tonalElevation = 5.dp) {
                 ReplyApp(
                     replyHomeUIState = uiState,
                     closeDetailScreen = {
